8c908a204ee16e646398e887f801bcf584e0cd77
ivan-lab-pvls <ivan.labasok@pvls.tech>
2024-04-02 18:22:19 +0200

    fix

--- lib/presentation/onboarding_screen/onboarding_screen.dart
+++ lib/presentation/onboarding_screen/onboarding_screen.dart
@@ -21,14 +21,14 @@ class _OnboardingScreenState extends State<OnboardingScreen> {
   List<OnboardingModel> pages = [
     OnboardingModel(
       text:
-      'Ah, Senet, come closer. I\'ve been thinking about the potions we discussed earlier. Have you made any progress?',
+          'Ah, Senet, come closer. I\'ve been thinking about the potions we discussed earlier. Have you made any progress?',
       name: 'Queen Nefertari',
       imagePath: ImageConstant.imgQueenOnboard,
     ),
     OnboardingModel(
       name: 'Senet, Royal Alchemist',
       text:
-      'Yes, Your Majesty. I\'ve been experimenting with a blend of lotus petals and myrrh resin for a potion to enhance vitality and beauty.',
+          'Yes, Your Majesty. I\'ve been experimenting with a blend of lotus petals and myrrh resin for a potion to enhance vitality and beauty.',
       imagePath: ImageConstant.imgAlchemistOnboard,
       reversePosition: true,
     ),
@@ -41,35 +41,35 @@ class _OnboardingScreenState extends State<OnboardingScreen> {
     OnboardingModel(
       name: 'Senet, Royal Alchemist',
       text:
-      'Initial trials have shown promising effects, Your Majesty. Subjects report increased energy and a noticeable improvement in skin complexion.',
+          'Initial trials have shown promising effects, Your Majesty. Subjects report increased energy and a noticeable improvement in skin complexion.',
       imagePath: ImageConstant.imgAlchemistOnboard,
       reversePosition: true,
     ),
     OnboardingModel(
       name: 'Queen Nefertari',
       text:
-      'Excellent news, Senet. But I wonder, can we refine it further? I desire a potion that not only enhances physical appearance but also sharpens the mind and soothes the spirit.',
+          'Excellent news, Senet. But I wonder, can we refine it further? I desire a potion that not only enhances physical appearance but also sharpens the mind and soothes the spirit.',
       imagePath: ImageConstant.imgQueenOnboard,
       reversePosition: false,
     ),
     OnboardingModel(
       name: 'Senet, Royal Alchemist',
       text:
-      'Of course, Your Majesty. I shall incorporate ingredients such as sacred blue lily and frankincense to achieve such effects. It will require delicate balance and precise brewing, but I am confident in my abilities.',
+          'Of course, Your Majesty. I shall incorporate ingredients such as sacred blue lily and frankincense to achieve such effects. It will require delicate balance and precise brewing, but I am confident in my abilities.',
       imagePath: ImageConstant.imgAlchemistOnboard,
       reversePosition: true,
     ),
     OnboardingModel(
       name: 'Queen Nefertari',
       text:
-      'Very well, Senet. Your dedication to your craft is admirable. I eagerly await the results of your endeavors. Potion-making truly is an art form, don\'t you think?',
+          'Very well, Senet. Your dedication to your craft is admirable. I eagerly await the results of your endeavors. Potion-making truly is an art form, don\'t you think?',
       imagePath: ImageConstant.imgQueenOnboard,
       reversePosition: false,
     ),
     OnboardingModel(
       name: 'Senet, Royal Alchemist',
       text:
-      'It is a privilege to serve you and contribute to your beauty and well-being through the wonders of alchemy.',
+          'It is a privilege to serve you and contribute to your beauty and well-being through the wonders of alchemy.',
       imagePath: ImageConstant.imgAlchemistOnboard,
       reversePosition: true,
     ),
@@ -82,7 +82,7 @@ class _OnboardingScreenState extends State<OnboardingScreen> {
     OnboardingModel(
       name: '',
       text:
-      'Queen Nefertari wants her potions as soon as possible! But Senet doesn’t have this time. Potions take too much time',
+          'Queen Nefertari wants her potions as soon as possible! But Senet doesn’t have this time. Potions take too much time',
       imagePath: '',
       reversePosition: true,
     ),
@@ -95,7 +95,7 @@ class _OnboardingScreenState extends State<OnboardingScreen> {
     OnboardingModel(
       name: 'Senet, Royal Alchemist',
       text:
-      'Please! I need someone to help! I’ll tech you how to brew excellent potions. You’’ ll be best in Egypt.',
+          'Please! I need someone to help! I’ll tech you how to brew excellent potions. You’’ ll be best in Egypt.',
       imagePath: ImageConstant.imgAlchemistOnboard,
       reversePosition: true,
     ),
@@ -114,7 +114,7 @@ class _OnboardingScreenState extends State<OnboardingScreen> {
     OnboardingModel(
       name: 'Senet, Royal Alchemist',
       text:
-      'On the top of the screen you’’ll see todays order. In a book you can read recipes.',
+          'On the top of the screen you’’ll see todays order. In a book you can read recipes.',
       imagePath: ImageConstant.imgAlchemistOnboard,
       reversePosition: true,
     ),
@@ -176,14 +176,14 @@ class _OnboardingScreenState extends State<OnboardingScreen> {
   Widget listPageItem(BuildContext context, OnboardingModel model) {
     return Stack(
       alignment:
-      model.reversePosition ? Alignment.bottomRight : Alignment.bottomLeft,
+          model.reversePosition ? Alignment.bottomRight : Alignment.bottomLeft,
       children: [
         if (model.imagePath != '')
           CustomImageView(
             imagePath: model.imagePath,
           ),
         Positioned(
-          top: 550,
+          top: MediaQuery.of(context).size.height * .65,
           left: 20,
           right: 20,
           child: Container(
@@ -210,8 +210,8 @@ class _OnboardingScreenState extends State<OnboardingScreen> {
                   model.text,
                   textAlign: model.imagePath != ''
                       ? model.reversePosition
-                      ? TextAlign.right
-                      : TextAlign.left
+                          ? TextAlign.right
+                          : TextAlign.left
                       : TextAlign.center,
                 ),
               ],
